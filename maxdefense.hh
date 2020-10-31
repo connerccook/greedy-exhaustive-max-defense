bee30be703ddd25ae04197e5f3c1b32c7551dd24
Peter <PeterLe522@csu.fullerton.edu>
2020-10-30 20:03:20 -0700

    Exhaustive search

--- maxdefense.hh
+++ maxdefense.hh
@@ -20,13 +20,13 @@
 #include <string>
 #include <vector>
 
-
+#include "timer.hh"
 // One armor item available for purchase.
 class ArmorItem
 {
 	//
 	public:
-		
+
 		//
 		ArmorItem
 		(
@@ -42,21 +42,21 @@ class ArmorItem
 			assert(!description.empty());
 			assert(cost_gold > 0);
 		}
-		
+
 		//
 		const std::string& description() const { return _description; }
 		double cost() const { return _cost_gold; }
 		double defense() const { return _defense_points; }
-	
+
 	//
 	private:
-		
+
 		// Human-readable description of the armor, e.g. "new enchanted helmet". Must be non-empty.
 		std::string _description;
-		
+
 		// Cost, in units of gold; Must be positive
 		double _cost_gold;
-		
+
 		// Defense points; most be non-negative.
 		double _defense_points;
 };
@@ -72,35 +72,35 @@ typedef std::vector<std::shared_ptr<ArmorItem>> ArmorVector;
 std::unique_ptr<ArmorVector> load_armor_database(const std::string& path)
 {
 	std::unique_ptr<ArmorVector> failure(nullptr);
-	
+
 	std::ifstream f(path);
 	if (!f)
 	{
 		std::cout << "Failed to load armor database; Cannot open file: " << path << std::endl;
 		return failure;
 	}
-	
+
 	std::unique_ptr<ArmorVector> result(new ArmorVector);
-	
+
 	size_t line_number = 0;
 	for (std::string line; std::getline(f, line); )
 	{
 		line_number++;
-		
+
 		// First line is a header row
 		if ( line_number == 1 )
 		{
 			continue;
 		}
-		
+
 		std::vector<std::string> fields;
 		std::stringstream ss(line);
-		
+
 		for (std::string field; std::getline(ss, field, '^'); )
 		{
 			fields.push_back(field);
 		}
-		
+
 		if (fields.size() != 3)
 		{
 			std::cout
@@ -109,13 +109,13 @@ std::unique_ptr<ArmorVector> load_armor_database(const std::string& path)
 				;
 			return failure;
 		}
-		
+
 		std::string
 			descr_field = fields[0],
 			cost_gold_field = fields[1],
 			defense_points_field = fields[2]
 			;
-		
+
 		auto parse_dbl = [](const std::string& field, double& output)
 		{
 			std::stringstream ss(field);
@@ -123,12 +123,12 @@ std::unique_ptr<ArmorVector> load_armor_database(const std::string& path)
 			{
 				return false;
 			}
-			
+
 			ss >> output;
-			
+
 			return true;
 		};
-		
+
 		std::string description(descr_field);
 		double cost_gold, defense_points;
 		if (
@@ -149,7 +149,7 @@ std::unique_ptr<ArmorVector> load_armor_database(const std::string& path)
 	}
 
 	f.close();
-	
+
 	return result;
 }
 
@@ -178,7 +178,7 @@ void sum_armor_vector
 void print_armor_vector(const ArmorVector& armors)
 {
 	std::cout << "*** Armor Vector ***" << std::endl;
-	
+
 	if ( armors.size() == 0 )
 	{
 		std::cout << "[empty armor list]" << std::endl;
@@ -195,7 +195,7 @@ void print_armor_vector(const ArmorVector& armors)
 				<< std::endl
 				;
 		}
-		
+
 		double total_cost, total_defense;
 		sum_armor_vector(armors, total_cost, total_defense);
 		std::cout
@@ -227,16 +227,16 @@ std::unique_ptr<ArmorVector> filter_armor_vector
 )
 {
 	std::unique_ptr<ArmorVector> filtered = std::make_unique<ArmorVector>();
-	//std::unique_ptr<ArmorVector> filtered (new ArmorVector); both work 
-	
+	//std::unique_ptr<ArmorVector> filtered (new ArmorVector); both work
+
 	for(auto armor : source){
 		if ( armor->defense() >= min_defense && armor->defense() <= max_defense){
-			filtered->push_back(armor);			
+			filtered->push_back(armor);
 			if (filtered->size() >= total_size){
 				break;
-			}	
+			}
 		}
-	} 
+	}
 
 	return filtered;
 }
@@ -254,27 +254,21 @@ std::unique_ptr<ArmorVector> greedy_max_defense
 )
 {
 	std::unique_ptr<ArmorVector> result = std::make_unique<ArmorVector>();
-	//std::unique_ptr<ArmorVector> result(new ArmorVector(armors));
-	//ArmorVector todo = armors;
 	std::unique_ptr<ArmorVector> todo(new ArmorVector(armors));
-	
-	//int result_cost = 0;
-	double result_cost = 0.0;
-	
+
+	double result_cost = 0.0; //Variable to keep track of the total armor cost
+
 	while (todo->size() != 0){
 		size_t max_armor_index = -1;
 	    std::shared_ptr<ArmorItem> max_armor = todo->at(0);
-		double max_armor_value = 0.0;	
-		
+		double max_armor_value = 0.0;
+
 		for(size_t i = 0; i < todo->size(); i++ ){
 			auto armor = todo->at(i);
-			
-			if((result_cost + armor->cost()) <= total_cost){
-			
-			//result->push_back(armor);
-			//result_cost += armor->cost();
+
+			if((result_cost + armor->cost()) <= total_cost){ //Condition to check if total armor cost goes over budget
 			double armor_value = armor->defense() / armor->cost();
-			if ( armor_value > max_armor_value )
+			if ( armor_value > max_armor_value ) //Compares the armor values
 				{
 					max_armor = armor;
 					max_armor_value = armor_value;
@@ -283,29 +277,13 @@ std::unique_ptr<ArmorVector> greedy_max_defense
 			}
 		}
 		if (max_armor_index == -1) { break;}
-		
-		result->push_back(max_armor);
-		result_cost += max_armor->cost();
-		todo->erase(todo->begin() + max_armor_index);
-	}
-	return result;
-	
-	//std::unique_ptr<ArmorVector> result = std::make_unique<ArmorVector>();
-	/*ArmorVector todo = armors;
-	int result_cost = 0;
-	while (todo.size() != 0){
 
-		for(auto armor : armors){
-			
-			if((result_cost + armor->cost()) <= total_cost){
-			result->push_back(armor);
-			result_cost += armor->cost();
-			}
-		}
+		result->push_back(max_armor); //Pushes best armor item into a vector
+		result_cost += max_armor->cost(); //Adds armor cost to total cost
+		todo->erase(todo->begin() + max_armor_index); //Removes the armor
 	}
+
 	return result;
-	*/
-	//return nullptr;
 }
 
 
@@ -322,41 +300,18 @@ std::unique_ptr<ArmorVector> exhaustive_max_defense
 {
 	const int n = armors.size();
 	assert(n < 64);
-	
+
 	std::unique_ptr<ArmorVector> bestVector = std::make_unique<ArmorVector>();
 	std::shared_ptr<ArmorItem> bestItem = nullptr;
-	
-
-	/*for (uint64_t bits = 0; bits < (pow(2.0,n)-1)){
-		ArmorVector candidates;
-		for(u_int64_t j = 0; j < n-1; j++){
-			if(((bits >> j)) & 1 == 1){
-				candidates.push_back(armors[j]);
-			}
-		}
-		
-	}
-	//bestVector->push_back(bestItem);
-	
-	for(auto candidate : armors){
+while(n<64){
+	for(auto candidate : armors){ //For each loop to traverse through each armor item
 		if(candidate->cost() <= total_cost){
-			if(bestItem == nullptr || (candidate->defense() > bestItem->defense())){
-				bestVector->push_back(candidate);
-				bestItem = candidate;
+			if(bestItem == nullptr || (candidate->defense() > bestItem->defense())){ //Compares the current item(if any) with the next item
+				bestVector->push_back(candidate); //Pushes candidate into best vector
+				bestItem = candidate; // New best item
 			}
 		}
-	}*/
-
-	
-	//return bestVector;
-	return nullptr;
+	}
 }
-
-
-
-
-
-
-
-
-
+	return bestVector;
+}
